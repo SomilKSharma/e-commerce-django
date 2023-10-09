bf9f108dd9e9030e98b57b51f6ede39077517380
Somil Sharma <somil81@icloud.com>
2023-10-09 16:01:50 +0530

    Altered template cart

--- core/templatetags/cart_template_tags.py
+++ core/templatetags/cart_template_tags.py
@@ -1,13 +1,22 @@
+# Import necessary modules and models
 from django import template
 from core.models import Order
 
+# Create a template library instance
 register = template.Library()
 
-
+# Define a custom template filter called 'cart_item_count'
 @register.filter
 def cart_item_count(user):
+    # Check if the user is authenticated
     if user.is_authenticated:
+        # Query the Order model to get all orders for the user that are not yet 'ordered'
         qs = Order.objects.filter(user=user, ordered=False)
+
+        # Check if there are any uncompleted orders
         if qs.exists():
+            # Return the count of items in the first uncompleted order
             return qs[0].items.count()
+
+    # Return 0 if the user is not authenticated or has no uncompleted orders
     return 0
