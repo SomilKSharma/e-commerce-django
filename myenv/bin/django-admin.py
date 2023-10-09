d9a5610ddad29865b54e355ca4fa4bc9005b828f
Somil Sharma <somil81@icloud.com>
2023-10-09 10:30:33 +0530

    Project e - commerce

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/somilsharma/Documents/GitHub/django-ecommerce/myenv/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
